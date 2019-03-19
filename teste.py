59709cab51832fe1c77783706e63556bae7aeed2
arruda.rafa.ra@gmail.com <arruda.rafa.ra@gmail.com>
2019-03-19 07:55:04 -0500

    exemplo de transformação em java

--- teste.py
+++ teste.py
@@ -1,1 +1,12 @@
-print("HUUUUUUUUUM")
+def rafa(palavra,procu):
+	cont = 0
+	for i in palavra:
+		if proc == i:
+			cont += 1
+
+	print("foram encontra apenas {} letra do {}". format(cont,proc))
+
+
+palavra = input("Informe a palavra:")
+proc = input("Informe a procura:")
+rafa(palavra, proc)
