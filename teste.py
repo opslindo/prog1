8659f819675d5d88315520b0adb74f464d51f4eb
arruda.rafa.ra@gmail.com <arruda.rafa.ra@gmail.com>
2019-03-22 17:55:24 -0500

    mudei

--- teste.py
+++ teste.py
@@ -4,27 +4,13 @@ y = [2,5,3,9,1,"a"]
 print set(x).intersection(y)'''
 
 #########################################
-def conjunto(opcao):
-    print("o seu nome: ")
-    print("OK!")
+nome = ['r','a','f','a']
+sobrenome = ['a','r','r','u','d','a']
+print("*Algoritmo de conjuntos* ")
+print("O que tem comum entre nome e sobrenome:")
+print("\n")
+print set(nome).intersection(sobrenome)
 
-def subconjunto(opcao):
-    print("As letras incomum tem!")
-
-#codigo principal
-print("Olá Doido")
-
-print('''            [1] conjunto
-            [2] Subconjunto''')
-
-opcao = int(input("Informe a opção:"))
-if opcao == 1:
-    conjunto(opcao)
-elif opcao == 2:
-    print("aaaaaa")
-
-        #l1 = ["r" , "a", "f", "a"]
-        #l2 = ["a","r","r", "u","d", "a"]
 
 
 
