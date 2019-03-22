54e5a6c4a9bd4d666ce3b0710c64cc3093e71c74
arruda.rafa.ra@gmail.com <arruda.rafa.ra@gmail.com>
2019-03-22 08:55:39 -0500

    Trab  mat

--- teste.py
+++ teste.py
@@ -1,4 +1,51 @@
-def rafa(palavra,procu):
+#trabalho inicial de Matematica Discretaa
+'''x = [1,2,3,"a",4,8]
+y = [2,5,3,9,1,"a"]
+print set(x).intersection(y)'''
+
+#########################################
+def conjunto(opcao):
+    print("o seu nome: ")
+    print("OK!")
+
+def subconjunto(opcao):
+    print("As letras incomum tem!")
+
+#codigo principal
+print("Olá Doido")
+
+print('''            [1] conjunto
+            [2] Subconjunto''')
+
+opcao = int(input("Informe a opção:"))
+if opcao == 1:
+    conjunto(opcao)
+elif opcao == 2:
+    print("aaaaaa")
+
+        #l1 = ["r" , "a", "f", "a"]
+        #l2 = ["a","r","r", "u","d", "a"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''def rafa(palavra,procu):
 	cont = 0
 	for i in palavra:
 		if proc == i:
@@ -9,4 +56,4 @@ def rafa(palavra,procu):
 
 palavra = input("Informe a palavra:")
 proc = input("Informe a procura:")
-rafa(palavra, proc)
+rafa(palavra, proc)'''
